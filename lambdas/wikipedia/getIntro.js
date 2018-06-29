99d4bb1ad41e02cd32661fef26ce45c1729b5d84
Hamik Mukelyan <hamik@caltech.edu>
2018-06-29 01:33:18 -0700

    Tidied up

--- lambdas/wikipedia/getIntro.js
+++ lambdas/wikipedia/getIntro.js
@@ -3,6 +3,6 @@ const resp = require('../lib/responseHelpers');
 
 exports.main = (event, context, callback) => {
     const article = event.pathParameters.article;
-
+    // TODO: add rpc call to get introduction of wikipedia article of given name
     return callback(null, resp.success(response));
 };
